87365a26dc10fbb50e0f4733f7d925a78b5f68b4
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-20 11:12:24 -0300

    criando estrategia de mascara de campo

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,19 @@
 // MASCARA DINHEIRO - FORMATANDO INPUT DO PREÇO
-const input = document.querySelector('input[name="price"]')
-input.addEventListener('keydown', function (e) {
-    setTimeout(function(){
-        let { value } = e.target
-    
+const Mask = {
+    apply(input, func) {
+        // deixando a função dinamica
+        setTimeout(function(){
+            input.value = Mask[func](input.value)
+        }, 1)
+    },
+    formatBRL(value) {
         // tirando tudo que nao é numero quando o usuario for digitar - expressao regular
         value = value.replace(/\D/g,"")
 
         // formatando o dado para real
-        value = new Intl.NumberFormat('pt-BR', {
+        return new Intl.NumberFormat('pt-BR', {
             style: 'currency', //ex:1.000,00
             currency: 'BRL' //R$
         }).format(value/100)
-
-        e.target.value = value
-    }, 1)
-})
+    }
+}

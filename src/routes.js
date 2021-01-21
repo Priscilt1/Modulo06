30e5d2ebb8a399199687def5617a0316285644f1
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-21 10:39:01 -0300

    funcionanlidade de deletar

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,8 @@ routes.get ('/products/:id/edit', ProductController.edit)
 
 routes.post('/products', ProductController.post)
 routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
+
 
 
 // ALIAS - ATALHO   

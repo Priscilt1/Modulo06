c9a66ba8461e02d190610a921ecd0f99cce3b01c
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-29 15:00:27 -0300

    removendo imagem do gerenciador

--- public/scripts.js
+++ public/scripts.js
@@ -25,7 +25,7 @@ const PhotosUpload = {
     handleFileInput(event) {
         const { files: fileList } = event.target
 
-        if (PhotoUpload.hasLimit(event)) return
+        if (PhotosUpload.hasLimit(event)) return
 
         // fazendo com que a fileList se transforme em um array 
         Array.from(fileList).forEach(file => {
@@ -48,6 +48,7 @@ const PhotosUpload = {
     // regras de limitações
     hasLimit (event) {
         const { uploadLimit } = PhotosUpload
+        const { files: fileList} = event.target
 
         // lenght quantidade/tamanho
         if (fileList.length > uploadLimit) {
@@ -63,10 +64,27 @@ const PhotosUpload = {
         const div = document.createElement('div')
         div.classList.add ('photo')
 
-        div.onclick = () => alert('remover a foto')
+        div.onclick = PhotosUpload.removePhoto
 
         div.appendChild(image)
 
+        div.appendChild(PhotosUpload.getRemoveButton())
+
         return div
+    },
+    getRemoveButton () {
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        return button
+    },
+    removePhoto(event){
+        // parentNode é quem esta a cima, nesse caso, a div com a class photo
+        const photoDiv = event.target.parentNode
+        // o children é a lista
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        photoDiv.remove()
     }
 }

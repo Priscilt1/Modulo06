4fee8e21a14bab527d32545d0a2a2a0488e4f20e
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-02-10 10:07:21 -0300

    Organizando categorias e filtros com REDUCE

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -47,8 +47,17 @@ module.exports = {
             const categories = products.map(product => ({
                 id: product.category_id,
                 name: product.category_name
-            }))
+                // o reduce precisa de uma callback function e falar qual resultado que quer. Nesse caso, será um array. O mesmo resultado será passado como argumento
+            })).reduce((categoriesFilterd, category) => {
+                
+                // Só coloquei mais um na categoria se o nome for diferente do que eu ja tenho impresso na tela
+                const found = categoriesFilterd.some(cat => cat.id == category.id)
 
+                if(!found)
+                    categoriesFilterd.push(category)
+
+                return categoriesFilterd
+            }, []) //sempre precisa retornar o objeto final no reduce. No caso, um objeto de categoria contendo o ID e o name
 
             return res.render("search/index", {products, search, categories})
         }
